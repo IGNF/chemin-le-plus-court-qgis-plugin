11af927e6c5c506cb16d426a5e8298dda60cdb62
gerome <gerome.pecheur@ign.fr>
2025-07-07 11:46:01 +0200

    commit initial

---
+++ constante.py
@@ -0,0 +1,53 @@
+LAYER_ROUTE = "troncon_de_route"
+
+TITRE_INTERFACE = "Contribution directe (ROUTE)"
+VERSION = "v1.0.0"
+
+# champs
+IDENTIFIANT = "id"
+NATURE = "nature"
+IMPORTANCE = "importance"
+SENS = "sens_de_circulation"
+NB_VOIES = "nombre_de_voies"
+LARGEUR = "largeur_de_chaussee"
+ITI_VERT = "itineraire_vert"
+ACCES = "acces_vehicule_leger"
+
+# attributs
+SANS_OBJET = "Sans objet"
+DOUBLE_SENS = "Double sens"
+SENS_DIRECT = "Sens direct"
+SENS_INVERSE = "Sens inverse"
+SENS_UNIQUE = "Sens unique"
+
+ACCES_LIBRE = "Libre"
+ACCES_IMPOSSIBLE = "Physiquement impossible"
+ACCES_RESTREINT = "Restreint aux ayants droit"
+RTE_2_CHAUSSEES = "Route à 2 chaussées"
+RTE_1_CHAUSSEE = "Route à 1 chaussée"
+RTE_EMPIERREE = "Route empierrée"
+ROND_POINT = "Rond-point"
+CHEMIN = "Chemin"
+SENTIER = "Sentier"
+
+
+CLEABS = "cleabs"
+
+FOND_DIAL = "background-color:#d3ddff"
+
+# 0 : bouton clické → fond rose
+# 1 : valeur de l'objet → fond vert
+# 2 : valeur par defaut → pas de fond
+# 3 : couleur fond label → fond bleu clair
+# 4 : couleur fond bouton validation (orange)
+# 5 : valeur possible pour les champs en fonction de la nature
+# 6 : valeur interdite
+CUSTOM_WIDGETS = ("background-color: #ff8080 ;font-weight: bold",
+                  "background-color: #2ab51a ;font-weight: bold",
+                  "background-color: None ;font-weight: bold",
+                  "background-color: #b2c3ff",
+                  "background-color: #df920d",
+                  "font-weight: bold",
+                  "background-color: None ;font-weight: bold"
+                  )
+
---
+++ fonction.py
@@ -0,0 +1,38 @@
+import os.path
+from qgis.PyQt.QtWidgets import QMessageBox
+from qgis.PyQt.QtCore import Qt
+
+from qgis.core import QgsCoordinateReferenceSystem, QgsProject
+
+import subprocess
+
+from .constante import *
+
+def afficheerreur(text, titre="Erreur"):
+    msg = QMessageBox()
+    msg.setStyleSheet(FOND_DIAL)
+    msg.setIcon(QMessageBox.Warning)
+    msg.setWindowTitle(titre)
+    msg.setStandardButtons(QMessageBox.Ok)
+    msg.setText(text)
+    msg.setWindowFlags(Qt.WindowStaysOnTopHint)
+    msg.exec()
+
+def affichemessageAvertissement(text, titre):
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Warning)
+    msg.setStyleSheet(FOND_DIAL)
+
+    msg.setWindowTitle(titre)
+    msg.setText(text)
+    btnAnnuler = msg.addButton("Annuler", QMessageBox.YesRole)
+    btnAnnuler.setStyleSheet("color:red ; font-weight: bold")
+    btnValider = msg.addButton("valider les modifications", QMessageBox.AcceptRole)
+    btnValider.setStyleSheet("color:green ; font-weight: bold")
+    msg.setWindowFlags(Qt.WindowStaysOnTopHint)
+    msg.exec()
+
+    if msg.clickedButton() == btnAnnuler:
+        return False
+    if msg.clickedButton() == btnValider:
+        return True
